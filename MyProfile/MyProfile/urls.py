7adb3809fe5c2922eb07dd1ed29db34e840542dc
alinowrouzii <alinowrouzii@gmail.com>
2022-01-31 00:28:22 +0330

    Apply some changes

--- MyProfile/MyProfile/urls.py
+++ MyProfile/MyProfile/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('auth.api.urls')),
+    path('', include('authentication.api.urls')),
 ]
